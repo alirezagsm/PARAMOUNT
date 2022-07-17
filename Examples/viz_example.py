adef4f95348653c76cea1d1462318857b2e2cc10
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 <alireza.ghasemi@utwente.nl>
2022-07-17 12:43:29 +0200

    combined plots, new plot parameters

--- Examples/viz_example.py
+++ Examples/viz_example.py
@@ -14,6 +14,7 @@ def main():
     varlist: list of variables to consider
     dt: time step of data acquisition in seconds
     bounds: domain bounds for visualization [xmin, xmax, ymin, ymax, resolution]
+    freq_max: maximum frequency of interest in PSD plots
     POD(): initialize a dask cluster on local system
 
     """
@@ -21,13 +22,28 @@ def main():
     path_viz = ".viz"
     modelist = range(11)
     dt = 4e-5
-    bounds = [-0.075, 0.075, -0.05, 0.43, 2.5e-4]
+    bounds = [-0.075, 0.075, -0.005, 0.25, 2.5e-4]
+    freq_max = 3000
     varlist = POD.get_folderlist(path_results_pod, boolPrint=True)
     variables = varlist
-    pod = POD()
+    pod = POD(show_dashboard=False)
     pod.set_time(dt)
-    pod.set_viz_params(dpi=600, linewidth=0.65, color="k", cmap="seismic")
-    pod.svd_viz(variables, modelist, bounds, path_results_pod, path_viz=path_viz)
+    pod.set_viz_params(dpi=600, linewidth=0.85, color="k", cmap="seismic")
+    pod.svd_viz(
+        variables,
+        modelist,
+        bounds,
+        path_results_pod,
+        path_viz=path_viz,
+        freq_max=freq_max,
+        dist_map=True,
+    )
+    pod.s_viz_combined(
+        variables,
+        maxmode=100,
+        path_results_pod=path_results_pod,
+        path_viz=path_viz,
+    )
 
 
 if __name__ == "__main__":
