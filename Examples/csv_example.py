b87da550682255d5712f3a9dab49a28ce2233564
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 <alireza.ghasemi@utwente.nl>
2022-08-23 18:12:21 +0200

    added examples

--- Examples/csv_example.py
+++ Examples/csv_example.py
@@ -4,7 +4,7 @@ from src.PARAMOUNT import POD
 def main():
     """
 
-    Read csv datasets and perform Proper Orthogonal Decomposition.
+    Read csv datasets and generate parquet database for desired variables.
 
     Parameters Used
     ----------
@@ -13,6 +13,7 @@ def main():
     skiprows: skip rows in the csv data
     i_start: index of the first csv file starting at 0
     i_end: index of the last csv file. None means all the files
+    coordinates: either 2D (xy) or 3D (xyz). "x", "y" and "z" names are reserved
     varlist: list of variables to consider
 
     POD(): initialize a dask cluster on local system
@@ -24,12 +25,14 @@ def main():
     skiprows = 5
     i_start = 1
     i_end = None
+    coordiantes = "2D"
     varlist = POD.read_csv_columns(path_csv, skiprows=skiprows, boolPrint=True)
     variables = varlist
-    pod = POD()
+    pod = POD(show_dashboard=True)
 
     pod.csv_to_parquet(
         variables,
+        coordiantes,
         path_csv,
         path_parquet=path_parquet,
         i_start=i_start,
@@ -37,7 +40,12 @@ def main():
         skiprows=skiprows,
     )
 
-    pod.client.shutdown()
+    POD.read_csv_sequential(
+        path_csv=".data2",
+        path_save=".signals",
+        skiprows=0,
+    )
+
     POD.check_parquet(path_parquet=path_parquet)
 
 
